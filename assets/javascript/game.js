34070e87459a51cb097659cf53e43224639b1d18
James Kemp <jameskemp2796@yahoo.com>
2016-04-28 14:29:13 -0400

    My game outputs random numbers for each gem. but it won't add it to the score.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,63 @@
-/*Psuedo Code
+// NukaCola Collector by James Kemp
 
-
-
-*/
 $(document).ready(function(){
-
-//Sets the random number at the start of the game
-function randomNumberFunction() {
-    var randomNumber = Math.floor((Math.random() * 101) + 19);
-    $("#randomNumber").html(randomNumber);
-}
-randomNumberFunction();
 
 //variables for win and lose
 
 var winCounter = 0;
 var loseCounter = 0;
-var yourScore = 0
+var yourScoreNumber = 0;
 
-// variables for each nuka cola
+// variables for each nuka cola (the gems in my game)
 
 var nuka1Number = Math.floor((Math.random() * 12) + 1);
 var nuka2Number = Math.floor((Math.random() * 12) + 1);
 var nuka3Number = Math.floor((Math.random() * 12) + 1);
 var nuka4Number = Math.floor((Math.random() * 12) + 1);
+
+//Sets the random number at the start of the game
+function randomNumberFunction() {
+    var randomNumber = Math.floor((Math.random() * 101) + 19);
+    $("#randomNumber").html(randomNumber);
+    $("#yourScore").html(yourScoreNumber);
+}
+randomNumberFunction();
 
 //start game will reset values I hope
 
 function startGame() {
-var nuka1Number = Math.floor((Math.random() * 12) + 1);
-var nuka2Number = Math.floor((Math.random() * 12) + 1);
-var nuka3Number = Math.floor((Math.random() * 12) + 1);
-var nuka4Number = Math.floor((Math.random() * 12) + 1);
-randomNumberFunction();
-
+	var nuka1Number = Math.floor((Math.random() * 12) + 1);
+	var nuka2Number = Math.floor((Math.random() * 12) + 1);
+	var nuka3Number = Math.floor((Math.random() * 12) + 1);
+	var nuka4Number = Math.floor((Math.random() * 12) + 1);
+	randomNumberFunction();
+	$("#yourScore").html(yourScoreNumber);
 
 }
+
+//functions that increase score
+
 function increaseYourScore1(){
-	$("#yourScore").html(nuka1Number);
+	yourScoreNumber + nuka1Number;
+	$("#yourScore").html(yourScoreNumber);
 }
 function increaseYourScore2(){
-	$("#yourScore").html(nuka2);
+	$("#yourScore").html(nuka2Number);
 }
 function increaseYourScore3(){
-	$("#yourScore").html(nuka3);
+	$("#yourScore").html(nuka3Number);
 }
 function increaseYourScore4(){
-	$("#yourScore").html(nuka4);
+	$("#yourScore").html(nuka4Number);
 }
+
+//on click will increase score 
 $("#nuka1").on("click", increaseYourScore1);
+$("#nuka2").on("click", increaseYourScore2);
+$("#nuka3").on("click", increaseYourScore3);
+$("#nuka4").on("click", increaseYourScore4);
+
+
+
 
 });
