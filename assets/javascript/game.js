9206d64672e2598ea2427a56452faec598490afa
James Kemp <jameskemp2796@yahoo.com>
2016-04-28 15:05:58 -0400

    My game outputs random numbers for each gem. but it won't add it to the score.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,62 +2,70 @@
 
 $(document).ready(function(){
 
-//variables for win and lose
+	//variables for win and lose
 
-var winCounter = 0;
-var loseCounter = 0;
-var yourScoreNumber = 0;
+	var winCounter = 0;
+	var loseCounter = 0;
+	var yourScoreNumber = 0;
 
-// variables for each nuka cola (the gems in my game)
+	// variables for each nuka cola (the gems in my game)
 
-var nuka1Number = Math.floor((Math.random() * 12) + 1);
-var nuka2Number = Math.floor((Math.random() * 12) + 1);
-var nuka3Number = Math.floor((Math.random() * 12) + 1);
-var nuka4Number = Math.floor((Math.random() * 12) + 1);
-
-//Sets the random number at the start of the game
-function randomNumberFunction() {
-    var randomNumber = Math.floor((Math.random() * 101) + 19);
-    $("#randomNumber").html(randomNumber);
-    $("#yourScore").html(yourScoreNumber);
-}
-randomNumberFunction();
-
-//start game will reset values I hope
-
-function startGame() {
 	var nuka1Number = Math.floor((Math.random() * 12) + 1);
 	var nuka2Number = Math.floor((Math.random() * 12) + 1);
 	var nuka3Number = Math.floor((Math.random() * 12) + 1);
 	var nuka4Number = Math.floor((Math.random() * 12) + 1);
+
+	//Sets the random number at the start of the game
+	function randomNumberFunction() {
+	    var randomNumber = Math.floor((Math.random() * 101) + 19);
+	    $("#randomNumber").html(randomNumber);
+	    $("#yourScore").html(yourScoreNumber);
+	}
 	randomNumberFunction();
-	$("#yourScore").html(yourScoreNumber);
 
-}
+	//functions that increase score
 
-//functions that increase score
+	function increaseYourScore1(){
+		yourScoreNumber++;
+		console.log(yourScoreNumber);
+		$("#yourScore").html(yourScoreNumber);
+		
+	}
 
-function increaseYourScore1(){
-	yourScoreNumber + nuka1Number;
-	$("#yourScore").html(yourScoreNumber);
-}
-function increaseYourScore2(){
-	$("#yourScore").html(nuka2Number);
-}
-function increaseYourScore3(){
-	$("#yourScore").html(nuka3Number);
-}
-function increaseYourScore4(){
-	$("#yourScore").html(nuka4Number);
-}
+	function increaseYourScore2(){
+		var nuka1Number = Math.floor((Math.random() * 12) + 1);
+		var yourScoreNumber = 0;
+		yourScoreNumber + nuka2Number;
+		console.log(yourScoreNumber);
+		$("#yourScore").html(yourScoreNumber);
+	}
 
-//on click will increase score 
-$("#nuka1").on("click", increaseYourScore1);
-$("#nuka2").on("click", increaseYourScore2);
-$("#nuka3").on("click", increaseYourScore3);
-$("#nuka4").on("click", increaseYourScore4);
+	function increaseYourScore3(){
+		$("#yourScore").html(nuka3Number);
+	}
 
+	function increaseYourScore4(){
+		$("#yourScore").html(nuka4Number);
+	}
 
+	//on click will increase score 
+	$("#nuka1").on("click", increaseYourScore1);
+	$("#nuka2").on("click", increaseYourScore2);
+	$("#nuka3").on("click", increaseYourScore3);
+	$("#nuka4").on("click", increaseYourScore4);
+
+	function checkIfWin(){
+		if (yourScoreNumber == randomNumber){
+			winCounter++;
+			$("#winScore").html("<h2>Wins:</h2>" + winsCounter);
+			alert("You win!");
+		}
+		else if (yourScoreNumber > randomNumber){
+			loseCounter++;
+			alert("You lose!");
+		}
+	}
+	checkIfWin();
 
 
 });
