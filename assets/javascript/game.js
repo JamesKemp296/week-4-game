1801166f3fc45f82753a06d2de2a4bd20a12c153
James Kemp <jameskemp2796@yahoo.com>
2016-04-28 15:16:22 -0400

    Finished 90% of the code. No errors. And nothing works...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -47,22 +47,34 @@ $(document).ready(function(){
 	function increaseYourScore4(){
 		$("#yourScore").html(nuka4Number);
 	}
-
+	
 	//on click will increase score 
 	$("#nuka1").on("click", increaseYourScore1);
 	$("#nuka2").on("click", increaseYourScore2);
 	$("#nuka3").on("click", increaseYourScore3);
 	$("#nuka4").on("click", increaseYourScore4);
 
+	//checks if you won and resets everything
+	
 	function checkIfWin(){
 		if (yourScoreNumber == randomNumber){
 			winCounter++;
 			$("#winScore").html("<h2>Wins:</h2>" + winsCounter);
 			alert("You win!");
+			var nuka1Number = Math.floor((Math.random() * 12) + 1);
+			var nuka2Number = Math.floor((Math.random() * 12) + 1);
+			var nuka3Number = Math.floor((Math.random() * 12) + 1);
+			var nuka4Number = Math.floor((Math.random() * 12) + 1);
+			var yourScoreNumber = 0;
 		}
 		else if (yourScoreNumber > randomNumber){
 			loseCounter++;
 			alert("You lose!");
+			var nuka1Number = Math.floor((Math.random() * 12) + 1);
+			var nuka2Number = Math.floor((Math.random() * 12) + 1);
+			var nuka3Number = Math.floor((Math.random() * 12) + 1);
+			var nuka4Number = Math.floor((Math.random() * 12) + 1);
+			var yourScoreNumber = 0;
 		}
 	}
 	checkIfWin();
